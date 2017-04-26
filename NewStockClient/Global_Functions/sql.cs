7c9bd1aafc17f69d7086ccc496fd231c530a37c3
Dylan Steele <dylansteele57@gmail.com>
2017-04-25 21:47:31 -0400

    Inital Commit
    Initial Commit that has a started login page.

---
+++ NewStockClient/Global_Functions/sql.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewStockClient.Global_Functions
+{
+    static class sql
+    {
+        public static bool CheckLogin(string HashedPass, string Username)
+        {
+            using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DB"].ToString()))
+            {
+
+                conn.Open();
+                string sql = "select Password_Hash FROM STOCK_USER where Username=@Username";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.Add(new SqlParameter("@Username", Username));
+
+                string DBHash = (string)cmd.ExecuteScalar();
+
+                if (DBHash == HashedPass)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ NewStockClient/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewStockClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Submitbtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Checks the Users credentials 
+            bool rtnVal = Global_Functions.sql.CheckLogin(Global_Functions.Encryption.HashPassword(Passwordtxt.Password), Usernametxt.Text);
+            if (rtnVal == true)
+            {
+                //Opens up the Client window
+                Client clnt = new Client();
+                //Hides the Login Panel
+                this.Visibility = Visibility.Hidden;
+                clnt.ShowDialog();
+                Close();
+            }
+        }
+
+        private void Newuserbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string str = (string)btn.Content;
+        }
+    }
+}
