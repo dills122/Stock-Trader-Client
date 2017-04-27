a52baab5173e13d421f4c0c04db5f4b19dbc6003
Dylan Steele <dylansteele57@gmail.com>
2017-04-26 23:21:44 -0400

    Started Work on Client Panel
    Started to create the trading client panel.

--- NewStockClient/Client.xaml.cs
+++ NewStockClient/Client.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,25 @@ namespace NewStockClient
         public Client()
         {
             InitializeComponent();
+            BindComboBox(BuyCB);
+            BindComboBox(SellCB);
+        }
+
+        public void BindComboBox(ComboBox ComboBx)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ToString()))
+            {
+                conn.Open();
+                string sql = "SELECT StockID, Stock_Text FROM STOCK_LIBRARY";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Stock");
+                ComboBx.ItemsSource = ds.Tables["Stock"].DefaultView;
+                ComboBx.DisplayMemberPath = ds.Tables["Stock"].Columns["Stock_Text"].ToString();
+                ComboBx.SelectedValuePath = ds.Tables["Stock"].Columns["StockID"].ToString();
+            }
+
         }
     }
 }
